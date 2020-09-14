0c91abb1f210d194980bd12637654ca78e6446fd
Kim-Ju-Yeop <kjy031104@gmail.com>
2020-09-14 11:30:19 +0900

    [Refactoring] InfoActivity

--- presentation/src/main/java/kr/hs/dgsw/stac/semo/view/view/InfoActivity.kt
+++ presentation/src/main/java/kr/hs/dgsw/stac/semo/view/view/InfoActivity.kt
@@ -12,17 +12,12 @@ import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 class InfoActivity : BaseActivity<ActivityInfoBinding, InfoViewModel>() {
 
-    private var laundryList = ArrayList<String>()
-
     override val viewModel: InfoViewModel
         get() = getViewModel(InfoViewModel::class)
 
     override fun init() {
-        laundryList = intent.getStringArrayListExtra("laundryList")!!
-
-        for((idx, laundry) in laundryList.withIndex()) {
-            getLaundryInfo(laundry,idx == laundryList.size - 1)
-        }
+        viewModel.laundryList = intent.getStringArrayListExtra("laundryList")!!
+        viewModel.getLaundryInfo()
     }
     override fun observerViewModel(){
         with(viewModel) {
@@ -30,20 +25,5 @@ class InfoActivity : BaseActivity<ActivityInfoBinding, InfoViewModel>() {
                 startActivityExtraWithFinish(Intent(applicationContext, AddActivity::class.java).putExtra("laundryList", laundryList))
             })
         }
-    }
-
-    private fun getLaundryInfo(laundry: String, isLast:Boolean) {
-        val fireStore = FirebaseFirestore.getInstance()
-        fireStore.collection("washerMethod").document(laundry)
-            .get()
-            .addOnCompleteListener {
-                if (it.isSuccessful) {
-                    val title = it.result!!.get("title").toString()
-                    val sub = it.result!!.get("sub").toString()
-                    viewModel.laundryInfoModelList.add(LaundryInfoModel(laundry, title, sub))
-
-                    if(isLast) viewModel.setLaundryInfoList()
-                }
-            }
     }
 }
--- presentation/src/main/java/kr/hs/dgsw/stac/semo/viewmodel/view/InfoViewModel.kt
+++ presentation/src/main/java/kr/hs/dgsw/stac/semo/viewmodel/view/InfoViewModel.kt
@@ -1,20 +1,48 @@
 package kr.hs.dgsw.stac.semo.viewmodel.view
 
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.firestore.FirebaseFirestore
 import kr.hs.dgsw.stac.domain.LaundryInfoModel
 import kr.hs.dgsw.stac.semo.base.BaseViewModel
 import kr.hs.dgsw.stac.semo.widget.SingleLiveEvent
 import kr.hs.dgsw.stac.semo.widget.recyclerview.adpater.LaundryInfoAdapter
+import java.lang.Exception
 
 class InfoViewModel : BaseViewModel() {
 
-    val laundryInfoModelList = ArrayList<LaundryInfoModel>()
-    val infoLaundryAdapter = LaundryInfoAdapter()
+    var laundryList = ArrayList<String>()
+    val laundryInfoList = ArrayList<LaundryInfoModel>()
+    val laundryInfoAdapter = LaundryInfoAdapter()
+
+    val firebaseFireStore = FirebaseFirestore.getInstance()
+
+    val onFailureData = MutableLiveData<Exception>()
 
     val onSaveEvent = SingleLiveEvent<Unit>()
 
-    fun setLaundryInfoList() {
-        infoLaundryAdapter.setList(laundryInfoModelList)
-        infoLaundryAdapter.notifyDataSetChanged()
+    fun getLaundryInfo() {
+        firebaseFireStore.collection("washerMethod")
+            .get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    for (document in task.result!!) {
+                        for (laundry in laundryList) {
+                            if (document.id == laundry) {
+                                laundryInfoList.add(LaundryInfoModel(laundry, document["title"].toString(), document["sub"].toString()))
+                            }
+                        }
+                    }
+                    setList()
+                }
+            }
+            .addOnFailureListener {
+                onFailureData.value = it
+            }
+    }
+
+    fun setList() {
+        laundryInfoAdapter.setList(laundryInfoList)
+        laundryInfoAdapter.notifyDataSetChanged()
     }
 
     fun saveEvent() {
